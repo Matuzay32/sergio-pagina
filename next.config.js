4b9670ad88061bcc088bf70ffe877fabd0b64305
linuxlite <eze.rey92@gmail.com>
2023-02-04 14:27:26 -0300

    probando

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-
+	// i18n: {
+	// 	locales: ["en", "es"],
+	// 	defaultLocale: "en",
+	// },
 	swcMinify: true,
 	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
 		ignoreDuringBuilds: true,
 	},
 };
+
+module.exports = nextConfig;
