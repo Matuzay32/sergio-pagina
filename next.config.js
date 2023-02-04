7bdf4ed6fb2123deec1632b9545146b8763ac1d2
linuxlite <eze.rey92@gmail.com>
2023-02-04 13:37:15 -0300

    solucionando para deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+	reactStrictMode: true,
+	// i18n: {
+	// 	locales: ["en", "es"],
+	// 	defaultLocale: "en",
+	// },
+	swcMinify: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+};
